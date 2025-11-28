4d1680700c4b977f45b078d20b3c33fa1fb2c7e0
Hanna <hanna@prevetfamily.com>
2025-11-28 09:15:17 +0100

    Update api endpoints in Login and Register with relative paths.

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -35,7 +35,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/api/users", {
+      const response = await fetch("/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
